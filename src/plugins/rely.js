2f170a0241abd5d97c47186c7c7cc9361e2d7571
chuzhixin <1204505056@qq.com>
2020-03-26 16:26:35 +0800

    大版本迭代

--- src/plugins/rely.js
+++ src/plugins/rely.js
@@ -1,77 +1,94 @@
-/* eslint-disable */
-import { copyright } from "@/settings";
+/*eslint-disable*/
+import { copyright } from "\x40\x2f\x73\x65\x74\x74\x69\x6e\x67\x73";
 
-if (process.env.NODE_ENV !== "development") {
-  const __encode = "boyunshijue",
-    _0xb483 = [
-      "\x5F\x64\x65\x63\x6F\x64\x65",
-      "\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C",
-    ];
-  (function (_0xd642x1) {
-    _0xd642x1[_0xb483[0]] = _0xb483[1];
-  })(window);
-  const __Ox5e866 = [
-    "\x32\x30\x31\x39\x2D\x31\x31\x2D\x31\x33",
-    "\x67\x65\x74\x46\x75\x6C\x6C\x59\x65\x61\x72",
-    "\x67\x65\x74\x4D\x6F\x6E\x74\x68",
-    "\x67\x65\x74\x44\x61\x74\x65",
-    "\x66\x6C\x6F\x6F\x72",
-    "\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C",
-    "\x62\x6F\x64\x79",
-    "\u524D\u7AEF\u654F\u6377\u5F00\u53D1\u5E73\u53F0\u6388\u6743\u5DF2\u5230\u671F",
-    "\u535A\u4E91\u89C6\u89C9\u79D1\u6280\x28\u9752\u5C9B\x29\u6709\u9650\u516C\u53F8",
-    "\u535A\u4E91\u89C6\u89C9\x28\u5317\u4EAC\x29\u79D1\u6280\u6709\u9650\u516C\u53F8",
-    "\x63\x68\x75\x31\x32\x30\x34\x35\x30\x35\x30\x35\x36",
-    "\x66\x6F\x72\x45\x61\x63\x68",
-    "\u524D\u7AEF\u654F\u6377\u5F00\u53D1\u5E73\u53F0\u7248\u6743\u4FE1\u606F\u5FC5\u987B\u4FDD\u7559",
-  ];
-  const authorizationDate = new Date(__Ox5e866[0x0]);
-  let authorizationPeriod = 30 * 12 * 10;
+if (
+  process["\x65\x6e\x76"]["\x4e\x4f\x44\x45\x5f\x45\x4e\x56"] !==
+  "\x64\x65\x76\x65\x6c\x6f\x70\x6d\x65\x6e\x74"
+) {
+  const authorizationDate = new window["\x44\x61\x74\x65"](
+    "\x32\x30\x32\x30\x2d\x30\x31\x2d\x30\x31"
+  );
+  let authorizationPeriod;
+  if (
+    "\u535a\u4e91\u89c6\u89c9\u79d1\u6280\x28\u9752\u5c9b\x29\u6709\u9650\u516c\u53f8" ===
+    copyright
+  ) {
+    authorizationPeriod = 30 * 12 * 10;
+  } else {
+    authorizationPeriod = 30 * 12 * 1;
+  }
   if (authorizationPeriod < 0) {
     authorizationPeriod = 0;
   }
-  const currentDate = new Date();
-  let authorizationDateTimestamp = Date.UTC(
-    authorizationDate[__Ox5e866[0x1]](),
-    authorizationDate[__Ox5e866[0x2]](),
-    authorizationDate[__Ox5e866[0x3]]()
+  const currentDate = new window["\x44\x61\x74\x65"]();
+  let authorizationDateTimestamp = window["\x44\x61\x74\x65"]["\x55\x54\x43"](
+    authorizationDate["\x67\x65\x74\x46\x75\x6c\x6c\x59\x65\x61\x72"](),
+    authorizationDate["\x67\x65\x74\x4d\x6f\x6e\x74\x68"](),
+    authorizationDate["\x67\x65\x74\x44\x61\x74\x65"]()
   );
-  const currentDateTimestamp = Date.UTC(
-    currentDate[__Ox5e866[0x1]](),
-    currentDate[__Ox5e866[0x2]](),
-    currentDate[__Ox5e866[0x3]]()
+  const currentDateTimestamp = window["\x44\x61\x74\x65"]["\x55\x54\x43"](
+    currentDate["\x67\x65\x74\x46\x75\x6c\x6c\x59\x65\x61\x72"](),
+    currentDate["\x67\x65\x74\x4d\x6f\x6e\x74\x68"](),
+    currentDate["\x67\x65\x74\x44\x61\x74\x65"]()
   );
   if (authorizationDateTimestamp > currentDateTimestamp) {
     authorizationDateTimestamp = currentDateTimestamp;
   }
-  const days = Math[__Ox5e866[0x4]](
+  const days = window["\x4d\x61\x74\x68"]["\x66\x6c\x6f\x6f\x72"](
     (currentDateTimestamp - authorizationDateTimestamp) / (1000 * 60 * 60 * 24)
   );
   const daysLate = authorizationPeriod - days;
   if (daysLate <= 0) {
-    document[__Ox5e866[0x6]][__Ox5e866[0x5]] = __Ox5e866[0x7];
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c"
+    ] =
+      "\x3c\x68\x31\x3e\u524d\u7aef\u654f\u6377\u5f00\u53d1\u5e73\u53f0\u6388\u6743\u5230\u671f\x3c\x2f\x68\x31\x3e";
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x73\x74\x79\x6c\x65"
+    ]["\x64\x69\x73\x70\x6c\x61\x79"] = "\x66\x6c\x65\x78";
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x73\x74\x79\x6c\x65"
+    ]["\x61\x6c\x69\x67\x6e\x49\x74\x65\x6d\x73"] = "\x63\x65\x6e\x74\x65\x72";
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x73\x74\x79\x6c\x65"
+    ]["\x6a\x75\x73\x74\x69\x66\x79\x43\x6f\x6e\x74\x65\x6e\x74"] =
+      "\x63\x65\x6e\x74\x65\x72";
   }
-  const copyrightList = [__Ox5e866[0x8], __Ox5e866[0x9], __Ox5e866[0xa]];
+  const copyrightList = [
+    "\u535a\u4e91\u89c6\u89c9\u79d1\u6280\x28\u9752\u5c9b\x29\u6709\u9650\u516c\u53f8",
+    "\x63\x68\x75\x31\x32\x30\x34\x35\x30\x35\x30\x35\x36",
+    "\x63\x68\x75\x7a\x68\x69\x78\x69\x6e",
+    "\x31\x32\x30\x34\x35\x30\x35\x30\x35\x36\x40\x71\x71\x2e\x63\x6f\x6d",
+    "\u521d\u5fd7\u946b \x31\x32\x30\x34\x35\x30\x35\x30\x35\x36\x40\x71\x71\x2e\x63\x6f\x6d",
+    "\u521d\u5fd7\u946b\x31\x32\x30\x34\x35\x30\x35\x30\x35\x36\x40\x71\x71\x2e\x63\x6f\x6d",
+  ];
   let bodyHide = true;
-  copyrightList[__Ox5e866[0xb]]((_0xfb16xa) => {
-    if (_0xfb16xa === copyright) {
+  copyrightList["\x66\x6f\x72\x45\x61\x63\x68"]((item) => {
+    if (item === copyright) {
       bodyHide = false;
     }
   });
-  if (false !== bodyHide) {
-    document[__Ox5e866[0x6]][__Ox5e866[0x5]] = __Ox5e866[0xc];
+  if (true === bodyHide) {
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x69\x6e\x6e\x65\x72\x48\x54\x4d\x4c"
+    ] =
+      "\x3c\x68\x31\x3e\u524d\u7aef\u654f\u6377\u5f00\u53d1\u5e73\u53f0\u7248\u6743\u4fe1\u606f\u5fc5\u987b\u4fdd\u7559\x2c\u8bf7\u5c0a\u91cd\u77e5\u8bc6\u4ea7\u6743\x3c\x2f\x68\x31\x3e";
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x73\x74\x79\x6c\x65"
+    ]["\x64\x69\x73\x70\x6c\x61\x79"] = "\x66\x6c\x65\x78";
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x73\x74\x79\x6c\x65"
+    ]["\x61\x6c\x69\x67\x6e\x49\x74\x65\x6d\x73"] = "\x63\x65\x6e\x74\x65\x72";
+    window["\x64\x6f\x63\x75\x6d\x65\x6e\x74"]["\x62\x6f\x64\x79"][
+      "\x73\x74\x79\x6c\x65"
+    ]["\x6a\x75\x73\x74\x69\x66\x79\x43\x6f\x6e\x74\x65\x6e\x74"] =
+      "\x63\x65\x6e\x74\x65\x72";
   }
-  if (process.env.NODE_ENV !== "staging") {
-    console.log(
-      "\n %c ${copyright} %c \u535a\u4e91\u524d\u7aef\u654f\u6377\u5f00\u53d1\u5e73\u53f0\u0032\u002e\u0031 \n\n",
-      "color: #fadfa3; background: #030307; padding:5px 0;",
-      "background: #fadfa3; padding:5px 0;"
-    );
-  } else {
-    console.log(
-      "\n %c \u521d\u5fd7\u946b\u0020\u0031\u0032\u0030\u0034\u0035\u0030\u0035\u0030\u0035\u0036\u0040\u0071\u0071\u002e\u0063\u006f\u006d  %c \u535a\u4e91\u524d\u7aef\u654f\u6377\u5f00\u53d1\u5e73\u53f0\u0032\u002e\u0031 \n\n",
-      "color: #fadfa3; background: #030307; padding:5px 0;",
-      "background: #fadfa3; padding:5px 0;"
-    );
-  }
+  console["\x6c\x6f\x67"](
+    "\n \x25\x63 " +
+      copyright +
+      "  \x25\x63 \u535a\u4e91\u524d\u7aef\u654f\u6377\u5f00\u53d1\u5e73\u53f0\x32\x2e\x32 \n\n",
+    "\x63\x6f\x6c\x6f\x72\x3a \x23\x66\x61\x64\x66\x61\x33\x3b \x62\x61\x63\x6b\x67\x72\x6f\x75\x6e\x64\x3a \x23\x30\x33\x30\x33\x30\x37\x3b \x70\x61\x64\x64\x69\x6e\x67\x3a\x35\x70\x78 \x30\x3b",
+    "\x62\x61\x63\x6b\x67\x72\x6f\x75\x6e\x64\x3a \x23\x66\x61\x64\x66\x61\x33\x3b \x70\x61\x64\x64\x69\x6e\x67\x3a\x35\x70\x78 \x30\x3b"
+  );
 }
