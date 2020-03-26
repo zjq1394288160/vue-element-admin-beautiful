f8380b19024b00b730606c81907d0f49fdf39033
chuzhixin <1204505056@qq.com>
2020-03-26 21:24:18 +0800

    添加全局换肤

--- src/layouts/components/NavBar/index.vue
+++ src/layouts/components/NavBar/index.vue
@@ -23,6 +23,11 @@
               class="right-icon"
               :icon="['fas', 'redo']"
             />
+            <byui-icon
+              @click="handleChangeTheme"
+              class="right-icon"
+              :icon="['fas', 'brush']"
+            />
             <el-avatar icon="el-icon-user-solid"></el-avatar>
             <el-tooltip effect="light" placement="bottom">
               <div slot="content">上次登录时间:{{ lastLoginTime }}</div>
@@ -37,6 +42,41 @@
         </el-col>
       </el-row>
     </byui-main>
+    <el-dialog
+      append-to-body
+      title="修改配色"
+      :visible.sync="dialogVisible"
+      width="20%"
+    >
+      <el-form>
+        <el-form-item label="菜单主题色">
+          <el-color-picker
+            :predefine="['#2a58ad', '#001529', '#f56c6c']"
+            show-alpha
+            v-model="colors.color1"
+          ></el-color-picker>
+        </el-form-item>
+        <el-form-item label="菜单选中色">
+          <el-color-picker
+            :predefine="['#22468a', '#1890ff', '#0fd59d']"
+            show-alpha
+            v-model="colors.color2"
+          ></el-color-picker>
+        </el-form-item>
+        <el-form-item label="标签主题色">
+          <el-color-picker
+            :predefine="['#1890ff', '#0fd59d', '#f56c6c']"
+            show-alpha
+            v-model="colors.color3"
+          ></el-color-picker>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleSaveColors">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -57,6 +97,12 @@ export default {
   data() {
     return {
       pulse: false,
+      dialogVisible: false,
+      colors: {
+        color1: null,
+        color2: null,
+        color3: null,
+      },
     };
   },
   computed: {
@@ -69,9 +115,38 @@ export default {
       "selectedTag",
     ]),
   },
+  mounted() {
+    this.$nextTick(() => {
+      const theme = localStorage.getItem("BYUI-THEME");
+      if (null !== theme) {
+        this.$set(this.colors, "color1", JSON.parse(theme).color1);
+        this.$set(this.colors, "color2", JSON.parse(theme).color2);
+        this.$set(this.colors, "color3", JSON.parse(theme).color3);
+        this.handleSaveColors();
+      }
+    });
+  },
   methods: {
     handleCollapse() {
       this.$store.dispatch("settings/changeCollapse");
+    },
+    handleChangeTheme() {
+      this.dialogVisible = true;
+    },
+    handleSaveColors() {
+      let color1 = this.colors.color1;
+      let color2 = this.colors.color2;
+      let color3 = this.colors.color3;
+      let style = document.createElement("style");
+      style.innerHTML = `
+      .top-bar-container, .top-bar-container .byui-main, .side-bar-container, .logo-container-vertical, .logo-container-horizontal, .el-menu, .el-menu-item, .el-submenu.is-active.is-opened, .el-submenu__title, .el-menu-item.is-active, .el-menu-item .is-active { background-color:${color1}!important; }
+      body .el-menu--horizontal .top-bar-item-container  .el-menu-item:hover, body .el-menu--horizontal .top-bar-item-container .el-menu-item.is-active, body .app-wrapper .side-bar-container .el-submenu .el-menu-item.is-active, body .app-wrapper .side-bar-container  .el-menu-item:hover,body .side-bar-container .el-menu .el-menu-item.is-active{ background-color:${color2}!important; }
+      .tags-view-item.router-link-exact-active.router-link-active.active{ background-color: ${color3}; border: 1px solid ${color3}; } `;
+      document.getElementsByTagName("head").item(0).appendChild(style);
+      localStorage.setItem(
+        "BYUI-THEME",
+        `{"color1":"${color1}","color2":"${color2}","color3":"${color3}"}`
+      );
     },
     async logout() {
       await this.baseConfirm(
