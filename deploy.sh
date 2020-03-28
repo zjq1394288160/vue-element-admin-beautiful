493ad1f4b121f580541fcdb8351636dbb50aad22
初志鑫 <1204505056@qq.com>
2020-03-28 17:00:29 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-git init
-git add -A
-git commit -m 'deploy'
-git push -f "https://${access_token}@github.com/chuzhixin/vue-element-admin-beautiful.git" master
-git push -f "https://${access_token}@gitee.com/chu1204505056/byui.git" master
-
-set -e
-cd -
 npm run build:stage
 cd dist
 touch .nojekyll
@@ -19,4 +11,10 @@ git push -f "https://${access_token}@gitee.com/chu1204505056/byui.git" master:gh
 git push -f "https://${access_token}@github.com/chuzhixin/vue-element-admin-beautiful.git" master:gh-pages
 cd -
 
+git init
+git add -A
+git commit -m 'deploy'
+git push -f "https://${access_token}@github.com/chuzhixin/vue-element-admin-beautiful.git" master
+git push -f "https://${access_token}@gitee.com/chu1204505056/byui.git" master
 
+cd -
