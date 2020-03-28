8a92d6cc0c305612f2fd951c27d80b8a9906c33c
初志鑫 <1204505056@qq.com>
2020-03-28 16:57:50 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -e
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f "https://${access_token}@github.com/chuzhixin/vue-element-admin-beautiful.git" master
+git push -f "https://${access_token}@gitee.com/chu1204505056/byui.git" master
+
+set -e
+cd -
 npm run build:stage
 cd dist
 touch .nojekyll
@@ -7,11 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 git push -f "https://${access_token}@gitee.com/chu1204505056/byui.git" master:gh-pages
-git push -f "https://${access_token}@e.coding.net/chu1204505056/vue-element-admin-beautiful.git" master:gh-pages
+git push -f "https://${access_token}@github.com/chuzhixin/vue-element-admin-beautiful.git" master:gh-pages
 cd -
-set -e
-git init
-git add -A
-git commit -m 'deploy'
-git push -f "https://${access_token}@gitee.com/chu1204505056/byui.git" master
+
 
