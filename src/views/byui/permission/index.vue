b6b7bbd1dcad8d13e4924f77d18d17ff6e03fd92
初志鑫 <1204505056@qq.com>
2020-04-03 15:31:30 +0800

    添加permission 演示

--- src/views/byui/permission/index.vue
+++ src/views/byui/permission/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="permission-container">
     <el-divider content-position="left">
-      intelligence模式,前端根据roles拦截路由
+      intelligence模式,前端根据roles拦截路由(演示环境,默认使用此方案)
     </el-divider>
 
     <el-form ref="form" :model="form" :inline="true">
@@ -14,7 +14,7 @@
 
       <el-form-item>
         <el-button @click="handleChangePermission" type="primary"
-          >切换权限
+        >切换权限
         </el-button>
       </el-form-item>
       <el-form-item label="当前账号拥有的权限">
@@ -23,48 +23,93 @@
     </el-form>
 
     <el-divider content-position="left">
-      all模式,路由以及view文件引入全部交给后端
+      all模式,路由以及view文件引入全部交给后端(权限复杂,且随时变更,建议使用此方案)
     </el-divider>
     <p>
-      settings.js配置authentication为all即可完全交由后端控制,mock中有后端接口示例,权限繁琐,有几十种权限的项目直接用这种
+      settings.js配置authentication为all即可完全交由后端控制,mock中有后端接口示例,权限繁琐,有几十种权限的项目直接用这种,
+      由于演示环境是mock数据模拟,可能无法展现此功能的配置,只做如下展示,便于您的理解
     </p>
+    <br />
+    <el-table
+      :data="tableData"
+      row-key="path"
+      border
+      default-expand-all
+      :tree-props="{ children: 'children', hasChildren: 'hasChildren' }"
+    >
+      <el-table-column prop="name" label="name"></el-table-column>
+      <el-table-column prop="path" label="path"></el-table-column>
+      <el-table-column prop="component" label="component"></el-table-column>
+      <el-table-column prop="redirect" label="redirect"></el-table-column>
+      <el-table-column prop="meta.title" label="标题"></el-table-column>
+      <el-table-column label="图标">
+        <template slot-scope="scope">
+          <span v-if="scope.row.meta">
+            <byui-icon :icon="['fas', scope.row.meta.icon]"></byui-icon>
+          </span>
+        </template>
+      </el-table-column>
+      <el-table-column label="是否固定">
+        <template slot-scope="scope">
+          <span v-if="scope.row.meta">
+            {{ scope.row.meta.affix }}
+          </span>
+        </template>
+      </el-table-column>
+      <el-table-column label="是否无缓存">
+        <template slot-scope="scope">
+          <span v-if="scope.row.meta">
+            {{ scope.row.meta.noCache }}
+          </span>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import { tokenTableName } from "@/settings";
+  import { mapGetters } from "vuex";
+  import { tokenTableName } from "@/settings";
+  import { getRouterList } from "@/api/router";
 
-export default {
-  name: "Permission",
-  components: {},
-  data() {
-    return {
-      form: {
-        permission: "",
-      },
-    };
-  },
-  computed: {
-    ...mapGetters(["roles"]),
-  },
-  created() {},
-  mounted() {
-    this.form.permission = this.roles[0];
-  },
-  methods: {
-    handleChangePermission() {
-      localStorage.setItem(
-        tokenTableName,
-        `byui-${this.form.permission}-accessToken`
-      );
-      location.reload();
+  export default {
+    name: "Permission",
+    components: {},
+    data() {
+      return {
+        form: {
+          permission: "",
+        },
+        tableData: [],
+      };
     },
-  },
-};
+    computed: {
+      ...mapGetters(["roles"]),
+    },
+    created() {
+      this.fetchData();
+    },
+    mounted() {
+      this.form.permission = this.roles[0];
+    },
+    methods: {
+      handleChangePermission() {
+        localStorage.setItem(
+          tokenTableName,
+          `byui-${this.form.permission}-accessToken`
+        );
+        location.reload();
+      },
+      fetchData() {
+        getRouterList().then((res) => {
+          this.tableData = res.data;
+        });
+      },
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
-.permission-container {
-}
+  .permission-container {
+  }
 </style>
